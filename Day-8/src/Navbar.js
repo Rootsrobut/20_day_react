aeedfa9b0bed2b11c9086bbdc0fc458c65f81337
Rootsrobut <sumitkumarofficial007@gmail.com>
2025-01-24 23:21:09 +0530

    setstate-css-react

---
+++ Day-8/src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>Title of Navbar</div>
+      <div>
+        <img src="" alt="Cart Icon" />
+        <sup>count</sup>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
